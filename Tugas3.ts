d2d062f3ee48fa41e928fe5d2132b67fd7342318
26ipay <149394246+26ipay@users.noreply.github.com>
2023-11-10 15:09:58 +0700

    Tugas Typescript

---
+++ Tugas3.ts
@@ -0,0 +1,15 @@
+// Problem 3 : Program Hellog + Nama
+
+let nama: string = "";
+let output: string = 'Hallo ${nama}! Saya Javascript, bahasa yang sangat menyenangnkan.';
+console.log(output);
+
+// Problem 4 : Menghitung Luas Segitiga
+
+let alas = 20;
+let tinggi = 25;
+let luas_segitiga = 0.5 * alas * tinggi;
+
+console.log(`output = ${luas_segitiga}`);
+
+// Problem 5 : Menghitung Luas 
